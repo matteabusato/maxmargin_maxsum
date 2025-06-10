e4501c67f1541eecc5fea39cca21b3b9a0af4705
matteabusato <busatomattea@gmail.com>
2025-06-10 18:34:43 +0200

    Simulations

--- simulationpy.py
+++ simulationpy.py
@@ -359,14 +359,14 @@ class MaxSum:
     
 def run_simulations(spd, sq):
     N = 1001
-    M = 600
+    M = 700
     THRESHOLD = 1e-4
     ITERATIONS = 10000
     SETTING_PHI_DOWN = spd   # 0: linear, 1: squared, 2: exponential
     SETTING_Q = sq          # 0: non-forced, 1: forced
     R = 0.001
 
-    file_name = "convergence_results/results_"
+    file_name = "new_results/results_"
     if SETTING_Q == 0:
         file_name += "non_"
     file_name += "forced_"
@@ -380,7 +380,7 @@ def run_simulations(spd, sq):
     print(file_name)
 
     for i in range(100):
-        seed = i + 500
+        seed = i + 600
         np.random.seed(seed)
 
         param = Parameters(
